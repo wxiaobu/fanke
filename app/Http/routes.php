c1f0239ef4d302c7a4d120aed5d00deddd089674
jh <2529699061@qq.com>
2017-10-25 09:12:45 +0800

    111

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,11 +15,19 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-
+// 后台主页隐士路由
 Route::controller('admin/index','Admin\IndexController');
-Route::controller('admin/login','Admin\LoginController');
+// 后台订单隐士路由
 Route::controller('admin/order','Admin\OrderController');
+// 后台商品隐士路由
 Route::controller('admin/product','Admin\ProductController');
+// 后台用户隐士路由
 Route::controller('admin/user','Admin\UserController');
+// 后台系统设置隐士路由
 Route::controller('admin/system','Admin\SystemController');
+// 后台广告管理隐士路由
 Route::controller('admin/advert','Admin\AdvertController');
+
+
+// 后台登陆隐士路由
+Route::controller('admin/login','Admin\LoginController');
